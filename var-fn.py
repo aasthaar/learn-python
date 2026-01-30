a26a3315d835bbcef97e79c85249d183194b295e
Aastha Rai <aastha.rai@cloudsufi.com>
2026-01-30 20:30:54 +0530

    pylint changes

--- var-fn.py
+++
@@ -1,7 +0,0 @@
-def choices():
-    my_choice= "rock"
-    comp_choice = "paper"
-    return comp_choice
-
-choices = choices()
-print(choices)
---
+++ var_Fn.py
@@ -0,0 +1,12 @@
+"""A simple function to demonstrate returning a value."""
+def choices():
+    """
+    RPS
+    """
+    # my_choice= "rock"
+    comp_choice = "paper"
+    return comp_choice
+
+CHOICES = choices()
+
+print (CHOICES)
