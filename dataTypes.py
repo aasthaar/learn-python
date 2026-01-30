b049f907753aef90e2a01a75e31ec875c153994b
Aastha Rai <aastha.rai@cloudsufi.com>
2026-01-30 19:39:03 +0530

    update dataTypes.py to include working of OR operator

--- dataTypes.py
+++ dataTypes.py
@@ -6,3 +6,8 @@ age = 21
 print(isinstance(age, int))
 
 # complex, set, float, bool, tuple, list, dict, range etc. are other data types in Python.
+
+# if 1st argument if false and we are using 'OR' operator then 2nd argument will be considered, else first one.
+print(False or []) #both false
+print(False or 1)
+print(1 or {})
