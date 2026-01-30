1658da3f092ecef4c14202c1614883a3adf5892f
Aastha Rai <aastha.rai@cloudsufi.com>
2026-01-30 19:32:36 +0530

    data types in py

---
+++ dataTypes.py
@@ -0,0 +1,8 @@
+name = "Aastha"
+print(type(name))
+print(isinstance(name, str))
+
+age = 21
+print(isinstance(age, int))
+
+# complex, set, float, bool, tuple, list, dict, range etc. are other data types in Python.
